bb877f7125519d1dcfcdf29ad60e7a3db2ec44fd
apriliainezz <apriliainez19@gmail.com>
2025-05-25 14:28:12 +0700

    ubah db

--- backend/database/crud_notes.sql
+++ backend/database/crud_notes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 24, 2025 at 03:06 PM
+-- Generation Time: May 25, 2025 at 09:25 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -49,7 +49,7 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `gender` varchar(10) NOT NULL,
-  `refresh_token` varchar(255) DEFAULT NULL,
+  `refresh_token` text DEFAULT NULL,
   `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
   `updatedAt` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -59,7 +59,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `password`, `email`, `gender`, `refresh_token`, `createdAt`, `updatedAt`) VALUES
-(1, 'Aprilia Inez', '$2b$10$CHavGZ1ExhiHnKozgxz7YOiAUoiouI3fS0LdZArD8mdGQTP39B1I2', 'inesaprilia04@gmail.com', 'Perempuan', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IkFwcmlsaWEgSW5leiIsImVtYWlsIjoiaW5lc2FwcmlsaWEwNEBnbWFpbC5jb20iLCJnZW5kZXIiOiJQZXJlbXB1YW4iLCJjcmVhdGVkQXQiOiIyMDI1LTA1LTI0VDEyOjQ0OjQzLjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDI1LTA1LTI0VDEyOjU1OjMwLjAwMFoiLC', '2025-05-24 12:44:43', '2025-05-24 12:56:43');
+(1, 'Aprilia Inez', '$2b$10$CHavGZ1ExhiHnKozgxz7YOiAUoiouI3fS0LdZArD8mdGQTP39B1I2', 'inesaprilia04@gmail.com', 'Perempuan', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IkFwcmlsaWEgSW5leiIsImVtYWlsIjoiaW5lc2FwcmlsaWEwNEBnbWFpbC5jb20iLCJnZW5kZXIiOiJQZXJlbXB1YW4iLCJjcmVhdGVkQXQiOiIyMDI1LTA1LTI0VDEyOjQ0OjQzLjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDI1LTA1LTI0VDEyOjU2OjQzLjAwMFoiLC', '2025-05-24 12:44:43', '2025-05-24 13:37:39');
 
 --
 -- Indexes for dumped tables
